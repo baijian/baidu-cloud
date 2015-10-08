650a4e0b9d7d1834b2974aa0794a81d52f1d6d53
柏健 <jian.baij@gmail.com>
2015-10-08 22:47:38 +0800

    add file upload method

--- Constants.py
+++ Constants.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 PCS_URL = 'https://pcs.baidu.com'
+PCS_UPLOAD_URL = 'https://c.pcs.baidu.com'
+
 PCS_COMMON_URI = '/rest/2.0/pcs/'
 
 PCS_USER_QUOTA_URI = 'quota'
+PCS_FILE_RELATE_URI = 'file'
 
 
 disk_unit = ['Byte', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB']
--- PCS.py
+++ PCS.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+"""
+Personal Cloud Storage
+"""
 import Utils
+import urllib
 import Constants
 import requests
 
@@ -16,6 +20,10 @@ def __get(url, params):
 
 
 def get_pcs_quota():
+    """
+    空间配额信息
+    :return:
+    """
     url = Constants.PCS_URL + Constants.PCS_COMMON_URI + Constants.PCS_USER_QUOTA_URI
     params = {
         'method': 'info',
@@ -32,3 +40,37 @@ def get_pcs_quota():
 def get_remote_file():
     url = Constants.PCS_URL + Constants.PCS_COMMON_URI
     pass
+
+
+def add_pcs_dir(path):
+    url = Constants.PCS_URL + Constants.PCS_COMMON_URI + Constants.PCS_FILE_RELATE_URI
+    params = {
+        'method': 'mkdir',
+        'access_token': ACCESS_TOKEN,
+        'path': path,
+    }
+    r = requests.post(url, params=params)
+    if r.status_code == requests.codes.ok:
+        return r.json()
+    else:
+        return False
+
+
+def upload_file_to_pcs(path, file_path, overwrite=False):
+    url = Constants.PCS_UPLOAD_URL + Constants.PCS_COMMON_URI + Constants.PCS_FILE_RELATE_URI
+    print url
+    params = {
+        'method': 'upload',
+        'access_token': ACCESS_TOKEN,
+        'path': path,
+    }
+    if overwrite:
+        params['ondup'] = 'overwrite'
+    else:
+        params['ondup'] = 'newcopy'
+    f = open(file_path, "rb")
+    r = requests.post(url + "?" + urllib.urlencode(params), files={'file': ('file', f)})
+    if r.status_code == requests.codes.ok:
+        return r.json()
+    else:
+        return False
